af415f72aa444a49d5ac6bac6c0d80df30f83c42
kasper-pawlowski <kapi28000@gmail.com>
2022-09-22 20:14:54 +0200

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Root from './Root';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <React.StrictMode>
-    <Root />
-    // </React.StrictMode>
+    <React.StrictMode>
+        <Root />
+    </React.StrictMode>
 );
